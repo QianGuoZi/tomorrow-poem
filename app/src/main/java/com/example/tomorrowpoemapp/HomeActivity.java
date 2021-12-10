766bada14bec98cec4e999c00806263930567466
QianGuoZi <lianghuaqian@163.com>
2021-12-10 16:04:28 +0800

    feat: 增加网络请求

--- app/src/main/java/com/example/tomorrowpoemapp/HomeActivity.java
+++ app/src/main/java/com/example/tomorrowpoemapp/HomeActivity.java
@@ -10,13 +10,26 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.text.TextPaint;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONArrayRequestListener;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Array;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -29,15 +42,17 @@ public class HomeActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
         tabbarFragment=TabbarFragment.newInstance();
         getSupportFragmentManager().beginTransaction().add(R.id.tabbar_fragment, tabbarFragment).commit();
 
-        cardFragment = CardFragment.newInstance("床前明月光\n疑是地上霜",3,true);
-        getSupportFragmentManager().beginTransaction().add(R.id.card_fragment, cardFragment).commit();
 
+        AndroidNetworking.initialize(getApplicationContext());
+        getContent();
         // 测试文本
 //        jumpToDetail=findViewById(R.id.jump_to_detail);
 //        jumpToDetail.setOnClickListener(new View.OnClickListener() {
@@ -51,12 +66,60 @@ public class HomeActivity extends FragmentActivity {
 
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        TextView content =  findViewById(R.id.card_content);
+//        content.setTypeface(Typeface.createFromAsset(getAssets(),"simsun.ttc"));
+//        TextPaint tp = content.getPaint();
+//        tp.setFakeBoldText(true);
+//    }
+
+    private void getContent(){
+
+        AndroidNetworking.get("https://service-eanmnyo2-1305624698.gz.apigw.tencentcs.com/release/api/poem/all")
+                .setPriority(Priority.HIGH)
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // do anything with response
+                        try{
+                            Integer status = response.getInt("status");
+                            String msg = response.getString("msg");
+                            JSONArray data = response.getJSONArray("data");
+                            Log.d("status",status.toString());
+                            Log.d("msg",msg);
+                            Log.d("data",data.toString());
+                            Log.d("sentence",data.getJSONObject(0).getString("sentence"));
+                            cardFragment = CardFragment.newInstance(data.getJSONObject(0).getString("sentence"),
+                                    data.getJSONObject(0).getInt("star"),true);
+                            FragmentManager fm = getSupportFragmentManager();
+                            fm.beginTransaction().add(R.id.card_fragment, cardFragment).commit();
+                            fm.executePendingTransactions();
+                            setFont();
+
+
+                        }catch (JSONException e){
+                            e.printStackTrace();
+                            Toast.makeText(HomeActivity.this, "Data error!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    @Override
+                    public void onError(ANError error) {
+                        // handle error
+                        Toast.makeText(HomeActivity.this,"Network error!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
+
+    private void setFont(){
         TextView content =  findViewById(R.id.card_content);
+        Log.d("content",content.toString());
         content.setTypeface(Typeface.createFromAsset(getAssets(),"simsun.ttc"));
         TextPaint tp = content.getPaint();
         tp.setFakeBoldText(true);
     }
+
 }
